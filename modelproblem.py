016b597ecfc4afdd4c19a8b9ef94658d0f764bff
LarkinIt <caroline.larkin@pitt.edu>
2024-12-23 12:05:28 -0500

    Test model import and compile fix

--- modelproblem.py
+++ modelproblem.py
@@ -17,14 +17,15 @@ class ModelProblem():
 
 		petab_problem = petab.v1.Problem.from_yaml(petab_yaml)
 		importer = pypesto.petab.PetabImporter(petab_problem)
+		problem = importer.create_problem(verbose=True)
 
-		model = importer.create_model(verbose=True, force_compile=True)
-		obj = importer.create_objective()
-		obj.amici_solver.setRelativeTolerance(1e-8)
-		obj.amici_solver.setAbsoluteTolerance(1e-12)
-		problem = importer.create_problem(obj, startpoint_kwargs={"check_fval": True})
+		#model = importer.create_model(verbose=True, force_compile=True)
+		#obj = importer.create_objective()
+		#obj.amici_solver.setRelativeTolerance(1e-8)
+		#obj.amici_solver.setAbsoluteTolerance(1e-12)
+		#problem = importer.create_problem(obj, startpoint_kwargs={"check_fval": True})
 	
-		self.model = model
+		#self.model = model
 		self.problem = problem
 		self.petab_problem = petab_problem
 		"""self.obj = obj
