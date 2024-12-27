bb42ba97dddef571a4274a7214dfae9af123baaf
LarkinIt <caroline.larkin@pitt.edu>
2024-12-26 20:17:27 -0500

    Added Raia visualization; updated ptmcmc slurm script for Raia

--- submit_ptmcmc_jobs.sh
+++ submit_ptmcmc_jobs.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 seeds=({0..99..1})
-declare -a probs=("Boehm_JProteomeRes2014")
+declare -a probs=("Raia_CancerResearch2011")
 declare -a methods=("ptmcmc")
 n_ensemble=1000
-n_iter=149750
+n_iter=615500
 ##### Declare anything else needed to pass as an arg below
-n_chains=4
+n_chains=8
 
 for prob in "${probs[@]}"; do
 	for method in "${methods[@]}"; do
